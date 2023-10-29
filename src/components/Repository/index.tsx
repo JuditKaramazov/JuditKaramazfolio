d8cc8e16a76bedc37ecf3aa8be78094c3fe2c551
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-10-29 05:46:55 +0100

    Update Repository components

---
+++ src/components/Repository/index.tsx
@@ -0,0 +1,48 @@
+import Repo from '../../interfaces/repo'
+import Icon from '../Icon'
+import styles from './index.module.css'
+
+export default function Repository({ repo }: { repo: Repo }) {
+  const { name, full_name, description, html_url, homepage, stargazers_count } =
+    repo
+
+  const isExternal = full_name.includes('JuditKaramazov')
+  if (isExternal) {
+    return null
+  }
+
+const repoLink =
+  name === 'blog' || name === 'portfolio' || !homepage
+    ? html_url
+    : homepage
+
+  return (
+    <div className={styles.repo}>
+      <h3 className={styles.repoTitle}>
+        <a href={repoLink}>{isExternal ? full_name : name}</a>
+      </h3>
+      <p>{description}</p>
+      <p className={styles.meta}>
+        {name === 'portfolio' || name === 'blog'
+          ? null
+          : !isExternal &&
+            homepage && (
+              <a href={homepage}>
+                <Icon name="Compass" /> More info
+              </a>
+            )}
+
+        <a href={html_url}>
+          <Icon name="GitHub" /> GitHub
+        </a>
+
+        <a
+          aria-label={`${stargazers_count} stars`}
+          href={`${html_url}/stargazers`}
+        >
+          <Icon name="Star" /> {stargazers_count}
+        </a>
+      </p>
+    </div>
+  )
+}
