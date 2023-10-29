cd18793402442947db2c2423ea5b542172d655db
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-10-29 05:46:15 +0100

    Update Overview component

---
+++ src/components/Overview/index.tsx
@@ -0,0 +1,34 @@
+import styles from './index.module.css'
+
+export default function Overview() {
+    const overviewStack = [
+        "HTML", 
+        "CSS", 
+        "SCSS", 
+        "JavaScript", 
+        "TypeScript", 
+        "React", 
+        "Next.js", 
+        "Jest", 
+        "Cafelito ☕️"
+    ]
+
+  return (
+    <>
+    <h2 className={styles.sectionTitle}>/overview</h2>
+    <div className={styles.projectTechstack}>
+        <ul>
+        {overviewStack.map((tech) => (
+            <li key={tech}>{tech}</li>
+        ))}
+        </ul>
+        <p className={styles.contentDownload}>
+            <a 
+                className={`${styles.iconDownload} ${styles.btn} ${styles.btnPrimary}`} 
+                href="/JuditKaramazovCV.pdf">Download CV
+            </a>
+        </p>
+    </div>
+    </>
+  )
+}
