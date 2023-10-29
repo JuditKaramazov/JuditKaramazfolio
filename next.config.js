c5cffdb7595e14a29af98e2a75134c24d08a888e
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-10-29 05:42:27 +0100

    Create Next app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,44 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// @ts-check
+
+const next = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    webpack: (config, options) => {
+      const fileLoaderRule = config.module.rules.find((rule) =>
+        rule.test?.test?.('.svg')
+      )
+
+      config.module.rules.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/
+        },
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          resourceQuery: { not: /url/ },
+          use: [{ loader: '@svgr/webpack', options: { icon: true } }]
+        }
+      )
+
+      fileLoaderRule.exclude = /\.svg$/i
+
+      config.module.rules.push({
+        test: /\.gif$/,
+        type: 'asset/resource'
+      })
+
+      return typeof defaultConfig.webpack === 'function'
+        ? defaultConfig.webpack(config, options)
+        : config
+    },
+    reactStrictMode: true
+  }
+
+  return nextConfig
 }
 
-module.exports = nextConfig
+export default next
