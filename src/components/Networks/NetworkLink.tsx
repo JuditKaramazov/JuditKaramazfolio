87294c1f261a7a0042f30fbec28d55fc538c4719
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-10-29 05:46:07 +0100

    Update Networks components

---
+++ src/components/Networks/NetworkLink.tsx
@@ -0,0 +1,24 @@
+import { LazyMotion, domAnimation, m } from 'framer-motion'
+import Icon from '../Icon'
+import { moveInTop } from '../Transitions'
+import styles from './index.module.css'
+
+export const NetworkLink = ({ name, url }: { name: string; url: string }) => {
+  const linkClasses =
+    name === 'Mail' ? `u-email ${styles.link}` : `u-url ${styles.link}`
+
+  return (
+    <LazyMotion features={domAnimation}>
+      <m.a
+        aria-label={name}
+        variants={moveInTop}
+        className={linkClasses}
+        href={url}
+        rel="me"
+      >
+        <Icon name={name} />
+        <span className={styles.title}>{name}</span>
+      </ m.a>
+    </ LazyMotion>
+  )
+}
---
+++ src/components/Networks/index.tsx
@@ -0,0 +1,49 @@
+import { LazyMotion, domAnimation, m, useReducedMotion } from 'framer-motion'
+import resume from '../../../_content/resume.json'
+import { getAnimationProps } from '../Transitions'
+import { NetworkLink } from './NetworkLink'
+import styles from './index.module.css'
+
+type Props = {
+  label: string
+  small?: boolean
+}
+
+const containerVariants = {
+  enter: {
+    transition: {
+      delay: 0.2,
+      staggerChildren: 0.1
+    }
+  }
+}
+
+export default function Networks({ label, small }: Props) {
+  const shouldReduceMotion = useReducedMotion()
+  const animationProps = getAnimationProps(shouldReduceMotion)
+
+  return (
+    <LazyMotion features={domAnimation}>
+      <m.section
+        aria-label={label}
+        variants={containerVariants}
+        {...animationProps}
+        className={small ? styles.small : styles.networks}
+      >
+        <NetworkLink
+          name="Mail"
+          key="Mail"
+          url={`mailto:${resume.basics.email}`}
+        />
+
+        {resume.basics.profiles.map((profile) => (
+          <NetworkLink
+            key={profile.network}
+            name={profile.network}
+            url={profile.url}
+          />
+        ))}
+      </ m.section>
+    </ LazyMotion>
+  )
+}
