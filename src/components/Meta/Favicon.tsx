bd2751036c4be6ae7cf4d1e10fd5b5d21dfdd5ce
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-10-29 05:45:53 +0100

    Update Meta components

---
+++ src/components/Meta/Favicon.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const MetaFavicons = () => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width,initial-scale=1"></meta>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    </ Head>
+  )
+}
+
+export default MetaFavicons
---
+++ src/components/Meta/index.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import meta from '../../../_content/meta.json'
+
+const Meta = ({
+  title,
+  description,
+  image,
+  slug
+}: {
+  title: string
+  description: string
+  image?: string
+  slug?: string
+}) => {
+  const url = slug ? `${meta.url}/${slug}` : meta.url
+
+  return (
+    <Head>
+      <link rel="canonical" href={url} />
+      <title>{title}</title>
+      <meta name="description" content={`${description.slice(0, 200)}...`} />
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={`${meta.url}/${image}`} />
+      <meta property="og:url" content={url} />
+      <meta name="logo:card" content="summary_large_image" />
+    </ Head>
+  )
+}
+
+export default Meta
