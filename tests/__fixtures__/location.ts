9c586071dcb9a1248ad8a628f69aa846ed0d756d
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-11-01 20:48:54 +0100

    Add tests/__fixtures__

---
+++ tests/__fixtures__/location.ts
@@ -0,0 +1,10 @@
+export const dataLocation = {
+    "now": {
+        "city": "Barcelona",
+        "country": "Spain",
+        "country_code": "ES",
+        "date_start": "2023-10-27",
+        "date_end": "2025-10-27",
+        "postalCode": "08820"
+    }
+}
